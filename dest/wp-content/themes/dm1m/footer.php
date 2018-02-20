5e1e42fcf9564fc6465f2b95c5ecef9a65240498
adrienlementheour <adrien.le.mentheour@gmail.com>
2018-02-20 15:08:14 +0100

    :art: icons

--- dest/wp-content/themes/dm1m/footer.php
+++ dest/wp-content/themes/dm1m/footer.php
@@ -5,5 +5,19 @@
 
         <?php wp_footer(); ?>
 
+        <svg aria-hidden='true' style='position: absolute; width: 0; height: 0; overflow: hidden;' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>
+            <defs>
+                <symbol id='icon-mail' viewBox='0 0 45 28'>
+                    <title>mail</title>
+                    <path d='M22.366 18.908l16.901-18.908h-33.812l16.911 18.908z'></path>
+                    <path d='M22.366 24.447c-1.592-0.027-2.997-0.805-3.879-1.994l-0.009-0.013-18.477-20.685v23.123c0 1.741 1.412 3.153 3.153 3.153v0h38.426c1.741 0 3.153-1.412 3.153-3.153v0-23.123l-18.477 20.685c-0.892 1.202-2.297 1.98-3.885 2.007l-0.004 0z'></path>
+                </symbol>
+                <symbol id='icon-facebook' viewBox='0 0 16 28'>
+                    <title>Facebook</title>
+                    <path d='M14.984 0.187v4.125h-2.453c-1.922 0-2.281 0.922-2.281 2.25v2.953h4.578l-0.609 4.625h-3.969v11.859h-4.781v-11.859h-3.984v-4.625h3.984v-3.406c0-3.953 2.422-6.109 5.953-6.109 1.687 0 3.141 0.125 3.563 0.187z'></path>
+                </symbol>
+            </defs>
+        </svg>
+        
         </body>
     </html>
