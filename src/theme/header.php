d39771fd7f774b11fd0f948753b24fdf36eb44b7
adrienlementheour <adrien.le.mentheour@gmail.com>
2018-02-20 16:09:27 +0100

    :art: header style

--- src/theme/header.php
+++ src/theme/header.php
@@ -14,15 +14,21 @@
 	</head>
 
 	<body <?php body_class(); ?>>
-
-		<header role='banner'>
-
-			<nav role='navigation'>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false, 'menu_class' => 'menu-main' ) ); ?>
-			</nav>
-
-			<a href='<?php echo home_url('/'); ?>' title='<?php bloginfo( 'name' ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a>
-
+	
+		<header class='header' role='banner'>
+			<div class='container'>
+				<div class='wrapper-logo'>
+					<a href='<?php echo home_url('/'); ?>' title='<?php bloginfo( 'name' ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a>
+				</div>
+				<nav class='menu' role='navigation'>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false, 'menu_class' => 'menu-main' ) ); ?>
+				</nav>
+				<div class='social'>
+					<a class='btn-social' href='#'>
+						<svg class='icon icon-facebook'><use xlink:href='#icon-facebook'></use></svg>
+					</a>
+				</div>
+			</div>
 		</header>
 
 		<main role='main'>
