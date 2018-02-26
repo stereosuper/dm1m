6b0bb75c064467931687de684734b01795b222d2
Clément Lemoine <clementlemoine.cl@gmail.com>
2018-02-26 16:46:15 +0100

    :bug: fix in/out too fast

--- src/js/gridHover.js
+++ src/js/gridHover.js
@@ -7,11 +7,28 @@ module.exports = function(grid){
     if(!grid.length) return;
     const tiles = grid.find('> article > a');
 
+    const getPos = (el) => {
+        let xPos = 0;
+        let yPos = 0;
+  
+        while (el) {
+          xPos += el.offsetLeft + el.clientLeft;
+          yPos += el.offsetTop + el.clientTop;
+  
+          el = el.offsetParent;
+        }
+        return {
+          x: xPos,
+          y: yPos
+        };
+      };
+
     const getDir = (e, el) => {
+        const position = getPos(el[0]);
         const w = el.width(),
             h = el.height(),
-            x = (e.pageX - el.offset().left - (w / 2) * (w > h ? (h / w) : 1)),
-            y = (e.pageY - el.offset().top - (h / 2) * (h > w ? (w / h) : 1)),
+            x = (e.pageX - position.x - (w / 2) * (w > h ? (h / w) : 1)),
+            y = (e.pageY - position.y - (h / 2) * (h > w ? (w / h) : 1)),
             d = Math.round( Math.atan2(y, x) / 1.57079633 + 5 ) % 4;
 
         return d;
@@ -29,7 +46,9 @@ module.exports = function(grid){
         });
 
         let where;
-        const tl = new TimelineMax();
+        const tlIn = new TimelineMax();
+        const tlOut = new TimelineMax();
+        
 
         switch ( direction ) {
             case 0 : 
@@ -51,19 +70,15 @@ module.exports = function(grid){
         }
 
         if(state === 'out'){
-            where.delay = 0.1;
-            where.ease = Power2.easeOut;
-            tl.add([
-                TweenMax.to(clouds, 0.6, {scale: 0.05, ease: Power2.easeOut}),
-                TweenMax.staggerTo(clouds, 0.6, where, 0.05)
-            ]);
+            TweenMax.killTweensOf(clouds);
+            TweenMax.to(clouds, 0.6, {scale: 0.05, delay: 0.05});
+            TweenMax.staggerTo(clouds, 0.6, where, 0.1);
             console.log('out');
         }else{
-            
-
-            tl.set(clouds, {opacity: 1})
-            .staggerFromTo(clouds, 0.6, where, {y: 0, x: 0, ease: Power2.easeOut}, 0.05)
-            .to(clouds, 0.6, {scale: 1.8, delay: -0.6, ease: Power2.easeOut});
+            TweenMax.killTweensOf(clouds);
+            TweenMax.set(clouds, {opacity: 1});
+            TweenMax.staggerFromTo(clouds, 0.6, where, {y: 0, x: 0, delay: 0.05}, 0.1);
+            TweenMax.to(clouds, 0.6, {scale: 1.8, delay: 0.2});
             console.log('in');
         }    
     };
