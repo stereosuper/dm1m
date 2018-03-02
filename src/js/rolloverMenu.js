0b908bc14612ac4177ca1b578a4ee76e387bcb64
adrienlementheour <adrien.le.mentheour@gmail.com>
2018-03-02 14:04:36 +0100

    :art: rollover to change sheep’s face preparation

--- src/js/rolloverMenu.js
+++ src/js/rolloverMenu.js
@@ -12,6 +12,8 @@ module.exports = function( nav ){
     
     const current = nav.find('.current-menu-item'), bubbles = nav.find('.bubble');
     const indicator = nav.find('.js-indic'), indicatorSemiWidth = indicator.width()/2;
+    const illus = $('.wrapper-logo .wrapper-illus');
+    const leftEye = illus.find('.left-eye'), rightEye = illus.find('.right-eye'), mouth = illus.find('.mouth');
 
 
     function moveBubble( bubble, distance, scale, timing ){
@@ -40,6 +42,22 @@ module.exports = function( nav ){
         }});
     }
 
+    function activateSheep(indexHovered){
+        switch (indexHovered) {
+            case 0:
+                // new
+                break;
+            case 1:
+                // projects
+                break;
+            case 2:
+                // agency
+                break;
+            default:
+                break;
+        }
+    }
+
 
     if( current.length ){
         current.data({'x': (current.position().left + current.width()/2) - indicatorSemiWidth});
@@ -54,7 +72,7 @@ module.exports = function( nav ){
     nav.on('mouseenter focusin', 'a', function(){
         TweenLite.to(bubbles, 0.3, {scale: 1});
         moveIndic( ($(this).parents('li').position().left + $(this).parents('li').width()/2) - indicatorSemiWidth, 0.6 );
-
+        activateSheep($(this).parents('li').index());
     }).on('mouseleave focusout', 'a', function(){
         current.length ? moveIndic( current.data('x'), 0.4 ) : TweenLite.to(bubbles, 0.3, {scale: 0});
 
