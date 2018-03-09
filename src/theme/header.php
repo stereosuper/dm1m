b33273dbb7a259e5551f6ee49c9039747d235604
Alphability <albanmezino@gmail.com>
2018-03-09 15:45:14 +0100

    :art: Setting favicon as smiling sheep head

--- src/theme/header.php
+++ src/theme/header.php
@@ -10,6 +10,14 @@
 
 		<?php wp_head(); ?>
 
+
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/site.webmanifest">
+		<meta name="msapplication-TileColor" content="#ededed">
+		<meta name="theme-color" content="#ffffff">
+
 		<script>document.getElementsByTagName('html')[0].className = 'js';</script>
 	</head>
 
