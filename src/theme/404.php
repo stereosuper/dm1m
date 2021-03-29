e323ee9f2683396b64d8f75acd5c5c2b9a8d6ec2
Elisabeth Hamel <elisabethhamel@outlook.com>
2021-03-29 14:25:24 +0200

    404 + default links

--- src/theme/404.php
+++ src/theme/404.php
@@ -1,9 +1,12 @@
 <?php get_header(); ?>
 
-<div class='container'>
+<div class='container e404'>
 
 	<h1>404</h1>
-	<p><?php _e('Page not found'); ?></p>
+	<p>Nous sommes désolés, cette page n'existe pas !</p>
+	<p><a href='<?php echo site_url(); ?>'>Retour à l'accueil</a></p>
+	<br>
+	<img src="https://media.giphy.com/media/2wfDH6nZQ58DS/source.gif" alt=""/>
 
 </div>
 
